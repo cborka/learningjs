01b1114f80cfee036daf6936c6d281762d5fb992
Boris Ts. (cborka) <cborka777@yandex.ru>
2021-01-22 16:21:02 +0700

    2021-01-22 закончил главу "Основы JS"

--- public/js/learnjs1.js
+++ public/js/learnjs1.js
@@ -26,5 +26,27 @@ const JS_TYPES =  "В JavaScript есть 8 основных типов. \n\
 // let result = prompt("Input: ", "123");
 // alert(result);
 
-let isTrue = confirm("Is it true?");
-alert (isTrue);
+// let isTrue = confirm("Is it true?");
+// alert (isTrue);
+
+// Преобразование типов
+const TYPE_CONVERSIONS = "Строковое String(value) \n\
+Числовое Number(value) или +value \n\
+// undefined -> NaN \n\
+// null -> 0 \n\
+// true/false -> 1/0 \n\
+// srtring -> Пробельные символы по краям обрезаются (пробел, табуляция, перевод строки), \n\
+//            пустая строка -> 0 \n\
+//            не пустая строка -> число или NaN \n\
+\n\
+Логическое Boolean(value) или !!value \n\
+// 0, NaN, undefined, null, '' -> false \n\
+// иначе -> true ('0', ' ' -> true) \n\
+";
+// alert(TYPE_CONVERSIONS);
+
+// Оператаор объединения с null
+// result = a ?? b
+// всё равно что
+// result = (a !== null && a !== undefined) ? a : b;
+
